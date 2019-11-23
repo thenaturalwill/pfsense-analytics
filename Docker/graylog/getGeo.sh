1051c95fcae0b8f7e58ce3257c57390520ced447
MatthewJSalerno <matthew.salerno.j@gmail.com>
2019-11-22 23:43:42 -0500

    Updated the graylog image to pull in the GeoIP when built. To update the GeoIP all you need to do is rebuild the image (docker-compose build)

---
+++ Docker/graylog/getGeo.sh
@@ -0,0 +1,3 @@
+GRAYLOG_PLUGIN_DIR=/etc/graylog/server/
+curl --output ${GRAYLOG_PLUGIN_DIR}/mm.tar.gz https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz
+tar zxvf ${GRAYLOG_PLUGIN_DIR}/mm.tar.gz -C ${GRAYLOG_PLUGIN_DIR} --strip-components=1
